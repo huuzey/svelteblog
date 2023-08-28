64987342ba2cfa49df687fdc279c8957cb06f784
huuzey <weleladinsefa@gmail.com>
2023-08-28 11:23:41 +0300

    all changes at once

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Shakspeare';
+export const description = 'Sveltekit blog for poets';
+export const url = dev ? 'http://localhost:5173' : '';
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1>About</h1>
+<p>I do a lot of long walks on the beach.</p>
